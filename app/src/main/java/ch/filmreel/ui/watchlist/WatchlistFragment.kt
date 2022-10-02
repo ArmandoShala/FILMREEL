f2e29ceaa65deb8e1b0ba4705e23c9e470aa24e7
Armando (shalaar3) <shalaar3@students.zhaw.ch>
2022-10-02 13:44:27 +0200

    feat(Started Scoreboard): Started implementation of scoreboard #7

--- app/src/main/java/ch/filmreel/ui/watchlist/WatchlistFragment.kt
+++ app/src/main/java/ch/filmreel/ui/watchlist/WatchlistFragment.kt
@@ -28,10 +28,10 @@ class WatchlistFragment : Fragment() {
         _binding = FragmentWatchlistBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        watchlistViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textNotifications
+//        watchlistViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
