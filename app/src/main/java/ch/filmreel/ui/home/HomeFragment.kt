a34b1bfe2be576f803d01410eddf643cb0a00858
= <landrit.a@hotmail.com>
2022-10-25 18:14:33 +0200

    basic app structure done #6

--- app/src/main/java/ch/filmreel/ui/home/HomeFragment.kt
+++ app/src/main/java/ch/filmreel/ui/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import ch.filmreel.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -16,22 +15,20 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val homeViewModel =
-            ViewModelProvider(this)[HomeViewModel::class.java]
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun redirectToQuiz() {
+        val playButton = binding.playButton
+        playButton.setOnClickListener {
+
+        }
     }
 }
