aead2e4b5aca99577d57a074232ab652104f31e0
Armando <shalaar3@students.zhaw.ch>
2022-10-31 23:23:22 +0100

    refactor(Watchlist as RecyclerView): RecyclerView layout almost done, needs a bit fine tuning [GH-11]

--- app/src/main/java/ch/filmreel/ui/watchlist/WatchlistRecyclerViewAdapter.kt
+++ app/src/main/java/ch/filmreel/ui/watchlist/WatchlistRecyclerViewAdapter.kt
@@ -1,10 +1,13 @@
 package ch.filmreel.ui.watchlist
 
-import androidx.recyclerview.widget.RecyclerView
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import ch.filmreel.R
 import ch.filmreel.databinding.FragmentWatchlistItemBinding
 import ch.filmreel.model.Movie
 
@@ -15,7 +18,6 @@ class WatchlistRecyclerViewAdapter(private val values: List<Movie>) :
     RecyclerView.Adapter<WatchlistRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
         return ViewHolder(
             FragmentWatchlistItemBinding.inflate(
                 LayoutInflater.from(parent.context),
@@ -23,36 +25,62 @@ class WatchlistRecyclerViewAdapter(private val values: List<Movie>) :
                 false
             )
         )
-
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = values[position]
+        holder.thumbnail.setImageURI(assemblyUrlForThumbnail(movie.id))
         holder.title.text = movie.title
-        holder.year.text = movie.year.toString()
         holder.description.text = movie.description
+        holder.year.text = movie.year.toString()
         holder.duration.text = movie.duration.toString()
-        holder.seen = movie.seen
+        holder.checkmark.setImageResource(R.drawable.ic_baseline_check_circle_outline_24)
+        holder.checkmark.setColorFilter(determineCheckmarkColor(movie.seen))
+        holder.checkmark.setOnClickListener {
+            Toast.makeText(
+                holder.checkmark.context,
+                "${holder.checkmark.colorFilter}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     override fun getItemCount(): Int = values.size
 
+    private fun determineCheckmarkColor(seen: Boolean): Int {
+        return when (seen) {
+            true -> android.R.color.holo_green_dark
+            false -> android.R.color.holo_purple
+        }
+    }
+
+    private fun assemblyUrlForThumbnail(movieId: String): Uri {
+        return Uri.parse("https://m.media-amazon.com/images/M/${movieId}@._V1_.jpg")
+    }
+
     inner class ViewHolder(binding: FragmentWatchlistItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        var title : TextView = binding.movieTitle
-        var year : TextView = binding.movieYear
-//        var genre : TextView = binding.movieGenres
-        var description : TextView = binding.movieShortDescription
-        var duration : TextView = binding.movieDuration
-//        var rating : TextView = binding.movieRating
-//        var questions : MutableList<Question>
-        var seen: Boolean = false
-
+        var thumbnail: ImageView = binding.movieThumbnail
+        var title: TextView = binding.movieTitle
+        var description: TextView = binding.movieShortDescription
+        var year: TextView = binding.movieYear
+        var duration: TextView = binding.movieDuration
+        var checkmark: ImageView = binding.movieCheckmark
+        var seen: Boolean = binding.movieSeen.isActivated
 
         override fun toString(): String {
             return super.toString() + " '" + title.text + "'"
         }
+
+        init {
+            checkmark.setOnClickListener {
+                val movie = values[bindingAdapterPosition]
+                movie.seen = !movie.seen
+                checkmark.setColorFilter(determineCheckmarkColor(movie.seen))
+            }
+        }
+
     }
 
 }
