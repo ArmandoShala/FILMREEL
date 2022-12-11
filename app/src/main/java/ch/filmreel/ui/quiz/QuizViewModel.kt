63ee9b7436d8f70736bb93fe692683320f4bdfde
landrit <landrit.a@hotmail.com>
2022-12-11 10:44:59 +0100

    git lfs file removed

--- app/src/main/java/ch/filmreel/ui/quiz/QuizViewModel.kt
+++ app/src/main/java/ch/filmreel/ui/quiz/QuizViewModel.kt
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:94e659de604ef1657893d384a554a6de723e44bda5d7a3a92ca2fc7687e7face
-size 983
+package ch.filmreel.ui.quiz
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import ch.filmreel.db.AppDatabase
+import ch.filmreel.db.entity.Movie
+import ch.filmreel.db.repository.QuizRepository
+import ch.filmreel.model.QuizAndPlayerAndMovie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class QuizViewModel(application: Application) : AndroidViewModel(application) {
+    val getQuiz: LiveData<List<QuizAndPlayerAndMovie>>
+    private val quizRepository: QuizRepository
+
+
+    init {
+        val quizWithPlayersAndMovieDAO = AppDatabase.getDatabase(application).quizWithPlayersAndMovieDAO()
+        quizRepository = QuizRepository(quizWithPlayersAndMovieDAO)
+        getQuiz = quizRepository.quizWithMoviesAndPlayer
+    }
+}
