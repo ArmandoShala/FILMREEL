afda0e4a4b4916625a32d9bad7947e98120cbd4e
landrit <landrit.a@hotmail.com>
2022-12-11 16:38:16 +0100

    change eric to bts

--- app/src/main/java/ch/filmreel/ui/leaderboard/LeaderBoardFragment.kt
+++ app/src/main/java/ch/filmreel/ui/leaderboard/LeaderBoardFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:403f373c8a4757ea24054c02a48d63e760f47432656f708765f8e025e2cf0e8d
-size 1801
+oid sha256:fc58a14931bc286437a4416aeadb217324c28bfe062688aa827b5aa5e39b0fb8
+size 1793
